ddda7b5ee4342fa2a43d927d2b76724edaa85684
soffia1407 <sff140705@gmail.com>
2025-03-24 21:25:40 +0700

    Created an instance of Vue + added a search of the array for outputting cards

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '.wrapper',
+    data: {
+        cards: [
+            { title: 'Задача 1', column: 1 },
+            { title: 'Задача 2', column: 2 },
+            { title: 'Задача 3', column: 3 }
+        ]
+    }
+});

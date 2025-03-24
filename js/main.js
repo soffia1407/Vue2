0e4c976d75fa1c270cc46c5ea14abeb231cf1609
soffia1407 <sff140705@gmail.com>
2025-03-25 01:09:20 +0700

    Adding lists to cards

--- js/main.js
+++ js/main.js
@@ -5,32 +5,33 @@ new Vue({
     el: '.wrapper',
     data: {
         cards: [
-            { title: 'Задача 1', column: 1 },
-            { title: 'Задача 2', column: 2 },
-            { title: 'Задача 3', column: 3 }
-        ]
+            { title: 'Задача 1', column: 1, items: [], newItemText: '' },
+            { title: 'Задача 2', column: 2, items: [], newItemText: '' },
+            { title: 'Задача 3', column: 3, items: [], newItemText: '' }
+        ],
+        newItemText: ''
     },
     computed: {
         column1CardCount() {
-          return this.cards.filter(card => card.column === 1).length;
+            return this.cards.filter(card => card.column === 1).length;
         },
         column2CardCount() {
-          return this.cards.filter(card => card.column === 2).length;
+            return this.cards.filter(card => card.column === 2).length;
         }
-      },
+    },
     methods: {
-        addCard: function(column) {
+        addCard: function (column) {
             if (column === 1 && this.column1CardCount >= MAX_CARDS_COLUMN_1) {
-              alert('В первом столбце находится максимальное количество карточек!');
-              return;
+                alert('В первом столбце находится максимальное количество карточек!');
+                return;
             }
             if (column === 2 && this.column2CardCount >= MAX_CARDS_COLUMN_2) {
-              alert('Во втором столбце находится максимальное количество карточек!');
-              return;
+                alert('Во втором столбце находится максимальное количество карточек!');
+                return;
             }
-            this.cards.push({ title: 'Новая задача', column: column });
-          },
-          moveCard: function(card, column) {
+            this.cards.push({ title: 'Новая задача', column: column, items: [], newItemText: '' });
+        },
+        moveCard: function (card, column) {
             if (column === 1 && this.column1CardCount >= MAX_CARDS_COLUMN_1) {
                 alert('В первом столбце находится максимальное количество карточек!');
                 return;
@@ -46,6 +47,12 @@ new Vue({
             if (index > -1) {
                 this.cards.splice(index, 1);
             }
-        }
+        },
+        addItem: function (card) {
+            if (card.newItemText.trim() !== '') {
+                card.items.push({ text: card.newItemText });
+                card.newItemText = '';
+            }
+        },
     }
 });

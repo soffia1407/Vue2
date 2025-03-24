19c8241a6686e848acb0fea13caeefc7c65da2fc
soffia1407 <sff140705@gmail.com>
2025-03-24 23:40:00 +0700

    Limiting card  in columns

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const MAX_CARDS_COLUMN_1 = 3;
+const MAX_CARDS_COLUMN_2 = 5;
+
 new Vue({
     el: '.wrapper',
     data: {
@@ -7,11 +10,35 @@ new Vue({
             { title: 'Задача 3', column: 3 }
         ]
     },
-    methods: {
-        addCard: function (column) {
-            this.cards.push({ title: 'Новая задача', column: column });
+    computed: {
+        column1CardCount() {
+          return this.cards.filter(card => card.column === 1).length;
         },
-        moveCard: function (card, column) {
+        column2CardCount() {
+          return this.cards.filter(card => card.column === 2).length;
+        }
+      },
+    methods: {
+        addCard: function(column) {
+            if (column === 1 && this.column1CardCount >= MAX_CARDS_COLUMN_1) {
+              alert('В первом столбце находится максимальное количество карточек!');
+              return;
+            }
+            if (column === 2 && this.column2CardCount >= MAX_CARDS_COLUMN_2) {
+              alert('Во втором столбце находится максимальное количество карточек!');
+              return;
+            }
+            this.cards.push({ title: 'Новая задача', column: column });
+          },
+          moveCard: function(card, column) {
+            if (column === 1 && this.column1CardCount >= MAX_CARDS_COLUMN_1) {
+                alert('В первом столбце находится максимальное количество карточек!');
+                return;
+            }
+            if (column === 2 && this.column2CardCount >= MAX_CARDS_COLUMN_2) {
+                alert('Во втором столбце находится максимальное количество карточек!');
+                return;
+            }
             card.column = column;
         },
         deleteCard: function (card) {
